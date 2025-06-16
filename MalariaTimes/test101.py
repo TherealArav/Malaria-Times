ddc0dd4d134c4ef3fbb484c7859971df86cb398c
AravGoyal <aravzgoyal@gmail.com>
2025-06-16 12:10:20 +0400

    updateV7

--- MalariaTimes/test101.py
+++ MalariaTimes/test101.py
@@ -1,19 +1,18 @@
-# # libraries
-# import mysql.connector as msc
-# from subprocess import call
+import mysql.connector as msc
+from subprocess import call
 
-# myConnector = msc.connect(host = "localhost",user = "root",password = "MySql1234IsDaBest",database = "data12")  # connect() function
-# if myConnector.is_connected():  # checking whether communication has been established sucessfully
-#     print("Python enviorment version connected to MySQL database succefully!")
+myConnector = msc.connect(host = "localhost",user = "root",password = "MySql1234IsDaBest",database = "data12")  # connect() function
+if myConnector.is_connected():  # checking whether communication has been established sucessfully
+    print("Python enviorment version connected to MySQL database succefully!")
 
-# myCursor = myConnector.cursor()
+myCursor = myConnector.cursor()
 
-# userInput = input("USERNAME: ")
-# passInput = input("PASSWORD: ")
-# myCursor.execute(f"INSERT INTO USERNAMES VALUES('{userInput}','{passInput}');")
-# myConnector.commit()
-# myConnector.close()             
-# call(["python","Malaria.py"])
+userInput = input("USERNAME: ")
+passInput = input("PASSWORD: ")
+myCursor.execute(f"INSERT INTO USERNAMES VALUES('{userInput}','{passInput}');")
+myConnector.commit()
+myConnector.close()             
+call(["python","Malaria.py"])
 
 
 
